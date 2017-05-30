1e5dc67aa0a310cb50e3318c26d8ce85a5c1c506
perrydv <perry@Perrys-iMac.local>
2017-05-30 14:39:05 -0700

    experimental chain-rule indexing content works.

--- packages/nimble/R/BUGS_getDependencies.R
+++ packages/nimble/R/BUGS_getDependencies.R
@@ -63,7 +63,7 @@ gd_getDependencies_IDs <- function(graph, maps, nodes, omit, downstream) {
 
 gd_allNeighbors <- function(graph, nodes) stop("shouldn't be calling gd_allNeighbors any more")
 
-enhanceDepsForDerivs <- function(inputNodes, deps, maps) {
+enhanceDepsForDerivs <- function(inputNodes, deps, model) {
     ## exploratory idea for enhancing an ordered set of nodes with parents, by input order, indexed by which input node they correspond to.
     ## Example:
     ## say deps has [2, 4, 6]
@@ -77,22 +77,59 @@ enhanceDepsForDerivs <- function(inputNodes, deps, maps) {
     ## from input nodes, see edgesFrom2To and see edgesFrom2ParentExprID
     ## see if any of the To nodes are in deps and provide the parentExprID
 
-    depIndex_2_parentDepIndices <- vector(mode = 'list', length = length(nodes))
-    for(i in seq_along(nodes)) {
-        thisNode <- nodes[i]
+    if(!is.integer(inputNodes)) inputNodes <- model$modelDef$nodeName2GraphIDs(inputNodes)
+    depIDs <- if(!is.integer(deps)) model$modelDef$nodeName2GraphIDs(deps) else deps
+
+    maps <- model$modelDef$maps
+    
+    declIDs <- maps$graphID_2_declID[depIDs]
+    
+    depIndex_2_parentDepIndices <- lapply(declIDs, integer)
+
+    for(i in seq_along(depIDs)) {
+        thisNode <- depIDs[i]
         if(thisNode %in% inputNodes) {
-            depIndex_2_parentDepIndices[[i]] <- 'input'
-            next
+            depIndex_2_parentDepIndices[[i]] <- -which(inputNodes == thisNode) ## e.g. set -2 for 2nd input node
         }
         toNodes <- maps$edgesFrom2To[[ thisNode ]]
         parentExprIDs <- maps$edgesFrom2ParentExprID[[ thisNode ]]
-        from(iTo in seq_along(toNodes)) {
+        for(iTo in seq_along(toNodes)) {
             thisToNode <- toNodes[iTo]
-            if(thisToNode %in% deps) {
-                iThisNodeInDeps <- which(deps == thisToNode)
+            if(thisToNode %in% depIDs) {
+                iThisNodeInDeps <- which(depIDs == thisToNode)
                 thisParentExprID <- parentExprIDs[iTo]
                 depIndex_2_parentDepIndices[[iThisNodeInDeps]][ thisParentExprID ] <- i
             }
         }
+    }
+    list(deps, depIndex_2_parentDepIndices)
+}
+
+explainDerivContent <- function(enhancedDeps, model) {
+    writeLines('The following calculations would be done from this input:')
+    deps <- enhancedDeps[[1]]
+    depIDs <- if(!is.integer(deps)) model$modelDef$nodeName2GraphIDs(deps) else deps
+    declIDs <- model$modelDef$maps$graphID_2_declID[depIDs]
+    derivInfo <- enhancedDeps[[2]]
+    for(i in seq_along(depIDs)) {
+        thisDerivInfo <- derivInfo[[i]]
+        if(length(thisDerivInfo) > 0) {
+            if(all(thisDerivInfo < 0)) { ## initiating node
+                if(length(thisDerivInfo) > 1) stop('Problem with thisDerivInfo')
+                description <- paste0('Input parameter ', -thisDerivInfo)
+            } else {
+                used <- thisDerivInfo > 0
+                if(sum(used) > 0) {
+                    argumentNames <- lapply( m$modelDef$declInfo[[ declIDs[i] ]]$symbolicParentNodesReplaced, deparse)
+                    description <- paste0('Argument ', seq_along(thisDerivInfo)[used], ' (', argumentNames[used],') comes from calculation ', thisDerivInfo[used])
+                }
+                else
+                    description <- "(No arguments are from previous calculations)\n"
+            }
+        } else
+            description <- ""
+        BUGSline <- deparse(m$modelDef$declInfo[[ declIDs[i] ]]$codeReplaced)
+        output <- paste0(i,': ', deps[i], ' (from ', BUGSline, ')\n', paste0('\t', description, collapse = '\n'))
+        writeLines(output)
     }
 }
