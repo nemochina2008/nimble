c97abde08fab46608b89bf59a42ea8e5f755b951
perrydv <perry@Perrys-iMac.local>
2017-05-30 14:58:10 -0700

    added comments

--- packages/nimble/R/BUGS_getDependencies.R
+++ packages/nimble/R/BUGS_getDependencies.R
@@ -64,38 +64,54 @@ gd_getDependencies_IDs <- function(graph, maps, nodes, omit, downstream) {
 gd_allNeighbors <- function(graph, nodes) stop("shouldn't be calling gd_allNeighbors any more")
 
 enhanceDepsForDerivs <- function(inputNodes, deps, model) {
-    ## exploratory idea for enhancing an ordered set of nodes with parents, by input order, indexed by which input node they correspond to.
-    ## Example:
-    ## say deps has [2, 4, 6]
+    ## This function takes a set of dependencies and returns a list with the original dependencies and a
+    ## set of enhanced information needed for chain-ruling derivatives
+    ##
+    ## deps is a vector of nodes returned by model$getDependencies(inputNodes)
+    ## inputNodes should also be in the deps.  
     ## 
 
-    ## path to info: idea 1: go up first
-    ## graphID_2_declID
-    ## from the declID we 
-
-    ## idea 2: 
-    ## from input nodes, see edgesFrom2To and see edgesFrom2ParentExprID
-    ## see if any of the To nodes are in deps and provide the parentExprID
-
+    ## convert inputNodes and deps from character to integer IDs
     if(!is.integer(inputNodes)) inputNodes <- model$modelDef$nodeName2GraphIDs(inputNodes)
     depIDs <- if(!is.integer(deps)) model$modelDef$nodeName2GraphIDs(deps) else deps
 
     maps <- model$modelDef$maps
-    
+
+    ## get the BUGS declaration ID for every node
     declIDs <- maps$graphID_2_declID[depIDs]
-    
+
+    ## initialize the enhanced information
+    ## Elements of depIndex_2_parentDepIndices correspond to elements of deps
+    ## depIndex_2_parentDepIndices[[i]] will have one of two formats:
+    ##    (1) a single negative integer.  This gives the (-) index of inputNodes corresponding to this node.
+    ##     e.g. if inputNodes is c('x[1]', 'x[2]'), and these are elements 1 and 2 in deps, then
+    ##      depIndex_2_parentDepIndices[[1]] will be -1
+    ##      depIndex_2_parentDepIndices[[2]] will be -2
+    ##    (2) a vector of integers giving the calculation index of deps corresponding to each input parameter
+    ##     e.g. if deps[5] is y[3], whose first argument is beta and second argument is x[4], then
+    ##       depIndex_2_parentDepIndices[[2]] will be c(0, 3)
+    ##           The 0 means that beta is not part of deps
+    ##           The 3 means that x[4] is deps[3]
     depIndex_2_parentDepIndices <- lapply(declIDs, integer)
 
+    ## For each input depsID
     for(i in seq_along(depIDs)) {
         thisNode <- depIDs[i]
+        ## Check if it is an input node
         if(thisNode %in% inputNodes) {
             depIndex_2_parentDepIndices[[i]] <- -which(inputNodes == thisNode) ## e.g. set -2 for 2nd input node
         }
+        ## Follow its descendents that are also in deps
+        ## toNodes will be the children of thisNode
         toNodes <- maps$edgesFrom2To[[ thisNode ]]
+        ## parentExprIDs will be the argument ID that thisNode represents to each of its child nodes
         parentExprIDs <- maps$edgesFrom2ParentExprID[[ thisNode ]]
+        ## for each child node
         for(iTo in seq_along(toNodes)) {
             thisToNode <- toNodes[iTo]
+            ## Check if this child is in depIDs
             if(thisToNode %in% depIDs) {
+                ## Populate an entry in the resultss
                 iThisNodeInDeps <- which(depIDs == thisToNode)
                 thisParentExprID <- parentExprIDs[iTo]
                 depIndex_2_parentDepIndices[[iThisNodeInDeps]][ thisParentExprID ] <- i
@@ -106,6 +122,7 @@ enhanceDepsForDerivs <- function(inputNodes, deps, model) {
 }
 
 explainDerivContent <- function(enhancedDeps, model) {
+    ## This function 
     writeLines('The following calculations would be done from this input:')
     deps <- enhancedDeps[[1]]
     depIDs <- if(!is.integer(deps)) model$modelDef$nodeName2GraphIDs(deps) else deps
